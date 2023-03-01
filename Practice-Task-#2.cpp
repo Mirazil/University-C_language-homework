babfad92e81dbb010380b771183fe987f4fff488
Oleh Chudakov <olegchudakov9@gmail.com>
2023-03-01 11:57:43 +0200

    2-3 tasks

---
+++ Practice-Task-#2.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <ctime> 
+
+using namespace std;
+
+const int SIZE = 5; // розмірність масиву
+
+int main() {
+    int matrix[SIZE][SIZE];
+
+    // заповнюємо масив випадковими значеннями
+    srand(time(NULL));
+    for (int* row = &matrix[0][0]; row <= &matrix[SIZE - 1][SIZE - 1]; row++) {
+        *row = rand() % 21 - 10;
+    }
+
+    // виводимо масив та знаходимо головну діагональ
+    int* ptr = &matrix[0][0];
+    int sum_even = 0, sum_odd = 0;
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
+            std::cout.width(4);
+            std::cout << *ptr;
+
+            if (*ptr % 2 == 0) { // парне число
+                sum_even++;
+            }
+            else { // непарне число
+                sum_odd++;
+            }
+            ptr++;
+        }
+        std::cout << endl;
+    }
+
+    std::cout << "Number of even elements: " << sum_even << std::endl;
+    std::cout << "Number of odd elements: " << sum_odd << std::endl;
+
+    return 0;
+}
---
+++ Practice-Task-#3.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main() {
+    srand(time(NULL));
+    int n; 
+    std::cout << "Enter number of elemnts in array: ";
+    std::cin >> n;
+    float* arr = new float[n];
+    int positive = 0, negative = 0;
+    for (float* p = arr; p < arr + n; p++) {
+        *p = -2 + static_cast<float>(rand()) / (static_cast<float>(RAND_MAX / (4)));
+        std::cout << *p << " ";
+        if (*p > 0)
+            positive++;
+        else if (*p < 0)
+            negative++;
+    }
+    std::cout << std::endl;
+    std::cout << "Positive numbers: " << positive << std::endl;
+    std::cout << "Negative numbers: " << negative << std::endl;
+    delete[] arr; // звільняємо пам'ять, виділену під масив
+    return 0;
+}
