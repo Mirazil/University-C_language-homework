e39a2d6d5b01f6250dcf40da247291d236fcc376
Oleh Chudakov <olegchudakov9@gmail.com>
2023-03-01 11:15:46 +0200

    Practice-1
    Two tasks

---
+++ Practice-Task-#1.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+int main()
+{
+    int vector[] = { 3, -5, 7, 10, -4, 14, 5, 2, -13 };
+    int n = sizeof(vector) / sizeof(vector[0]);
+
+    int* min_ptr = &vector[0];
+
+    for (int i = 1; i < n; i++) {
+        if (*(vector + i) < *min_ptr) {
+            min_ptr = (vector + i);
+        }
+    }
+
+    std::cout << "Minimal element is: " << *min_ptr << std::endl;
+
+    return 0;
+}   
